4c69def900ad114f9a36f611683a68ca5baa2041
arsmlnkv <melnikov.arsene@gmail.com>
2023-11-21 12:40:02 +0100

    Added another helper function to wrap around data processing in base API class and added one more type of data to extract.

--- src/owimetadatabase_preprocessor/locations/io.py
+++ src/owimetadatabase_preprocessor/locations/io.py
@@ -1,5 +1,6 @@
 """Module to connect to the database API to retrieve and operate on locations data."""
 
+import numpy as np 
 import pandas as pd
 import plotly.express as px  # type: ignore
 
@@ -18,7 +19,7 @@ class LocationsAPI(API):
     (see ``owimetadatabase`` code).
     """
 
-    def get_projectsites(self, **kwargs):
+    def get_projectsites(self, **kwargs) -> dict[str, pd.DataFrame | bool]:
         """Get all available projects.
 
         :return:  Dictionary with the following keys:
@@ -29,7 +30,21 @@ class LocationsAPI(API):
         url_params = {**url_params, **kwargs}
         url_data_type = "/locations/projectsites/"
         output_type = "list"
-        resp = self.send_request(url_data_type, url_params)
-        df = self.output_to_df(resp)
-        df_add = self.postprocess_data(df, output_type)
+        df, df_add = self.process_data(url_data_type, url_params, output_type)
         return {"data": df, "exists": df_add["existance"]}
+
+    def get_projectsite_detail(self, projectsite, **kwargs) -> dict[str, pd.DataFrame | bool | np.int64]:
+        """Get details for a specific projectsite.
+
+        :param projectsite: Title of the projectsite
+        :return:  Dictionary with the following keys:
+            - 'id': id of the selected project site
+            - 'data': Pandas dataframe with the location data for each projectsite
+            - 'exists': Boolean indicating whether matching records are found
+        """
+        url_params = {"projectsite": projectsite}
+        url_params = {**url_params, **kwargs}
+        url_data_type = "/locations/projectsites/"
+        output_type = "single"
+        df, df_add = self.process_data(url_data_type, url_params, output_type)
+        return {"id": df_add["id"], "data": df, "exists": df_add["existance"]}
