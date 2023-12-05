4ef364a34f8a532d7e042cc8871a752f54dbf905
arsmlnkv <melnikov.arsene@gmail.com>
2023-12-05 22:47:36 +0100

    Added Postions class to data.

--- src/owimetadatabase_preprocessor/geometry/data.py
+++ src/owimetadatabase_preprocessor/geometry/data.py
@@ -41,3 +41,16 @@ class Material(object):
             "poisson_ratio": self.poisson_ratio,
             "young_modulus": self.young_modulus,
         }
+
+
+class Position(object):
+    def __init__(self, x=0, y=0, z=0, alpha=0, beta=0, gamma=0, reference_system="LAT"):
+        self.x = x
+        self.y = y
+        self.z = z
+
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+
+        self.reference_system = reference_system
