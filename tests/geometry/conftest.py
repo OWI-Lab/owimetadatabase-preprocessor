917af282dcaf88fe832ed8e4bcf365902efe634a
arsmlnkv <melnikov.arsene@gmail.com>
2024-01-03 16:40:49 +0100

    Fixed tests for private methods of OWT.

--- tests/geometry/conftest.py
+++ tests/geometry/conftest.py
@@ -1002,3 +1002,68 @@ def owt(data_mat_df, sab, owt_api, mock_owt_set_subassemblies, mock_owt_set_memb
     mat = [m.to_dict() for _, m in data_mat_df.iterrows()]
     owt = OWT(owt_api, mat, sab)
     return owt
+
+
+@pytest.fixture(scope="function")
+def sa_list():
+    cols = [
+        "id", "title", "description", "slug", 
+        "x_position", "y_position", "z_position", "vertical_position_reference_system", 
+        "subassembly_type", "source", "asset", "model_definition"
+    ]
+    data = [
+        {
+            "id": 651,
+            "title": "BBG01_TW",
+            "description": None,
+            "slug": "bbg01_tw", 
+            "x_position": 0.0,
+            "y_position": 0.0,
+            "z_position": 17000.0,
+            "vertical_position_reference_system": "LAT", 
+            "subassembly_type": "TW",
+            "source": "vestas_tower_dwg.pdf",
+            "asset": 341,
+            "model_definition": 5
+        },
+        {
+            "id": 855,
+            "title": "BBG01_TP",
+            "description": None,
+            "slug": "bbg01_tp",
+            "x_position": 0.0,
+            "y_position": 0.0,
+            "z_position": -2540.0,
+            "vertical_position_reference_system": "LAT", 
+            "subassembly_type": "TP",
+            "source": "NBW-513-004-Design Report - WTG Time Domain Fatigue Analysis",
+            "asset": 341,
+            "model_definition": 5
+        },
+        {
+            "id": 64,
+            "title": "BBG01_MP",
+            "description": None,
+            "slug": "bbg01_mp",
+            "x_position": 0.0,
+            "y_position": 0.0,
+            "z_position": -63400.0,
+            "vertical_position_reference_system": "LAT",
+            "subassembly_type": "MP",
+            "source": "NBW-513-004-Design Report - WTG Time Domain Fatigue Analysis",
+            "asset": 341,
+            "model_definition": 5
+        }
+    ]
+    return data
+
+
+@pytest.fixture(scope="function")
+def sa_df(sa_list):
+    return pd.DataFrame(sa_list)
+
+
+@pytest.fixture(scope="function")
+def sab_list(owt_api, data_mat_df, sa_list, mock_requests_get_buildingblocks_sa) -> SubAssembly:
+    sa = [SubAssembly(data_mat_df, sa_dict, owt_api) for sa_dict in sa_list]
+    return sa
--- tests/geometry/test_processing.py
+++ tests/geometry/test_processing.py
@@ -19,5 +19,16 @@ class  TestOWT:
     def test_init(self, owt, owt_init) -> None:
         assert_attributes(owt, owt_init)
 
-    def test_set_subassemblies_and_members(self, owt_api, data_mat_df, sab) -> None:
-        owt = OWT(owt_api, data_mat_df, sab)
+    def test_set_subassemblies_and_members(self, owt_api, data_mat_df, sa_df, sab_list, mock_requests_get_buildingblocks_sa) -> None:
+        owt = OWT(owt_api, data_mat_df, sa_df)
+        sa_type = ["TW", "TP", "MP"]
+        sab_dict = {sa_type[i]: sab_list[i] for i in range(len(sa_type))}
+        for i in range(len(sa_type)):
+            sab_dict[sa_type[i]].building_blocks
+        assert owt.sub_assemblies == sab_dict
+        pd_testing.assert_frame_equal(owt.tower_sub_assemblies, sab_dict["TW"].as_df(), rtol=1e-3, atol=1e-3)
+        pd_testing.assert_frame_equal(owt.tp_sub_assemblies, sab_dict["TP"].as_df(), rtol=1e-3, atol=1e-3)
+        pd_testing.assert_frame_equal(owt.mp_sub_assemblies, sab_dict["MP"].as_df(), rtol=1e-3, atol=1e-3)
+
+    def test_set_df_structure(self):
+        pass
