6a203fe346f0f4fd615fdee51ae43a97951afc54
arsmlnkv <melnikov.arsene@gmail.com>
2024-03-08 09:46:15 +0100

    Fixed the release script for the correct versioning.

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-version_old=$(git describe --tags $(git rev-list --tags --max-count=1))
+version_old=$(git tag --list '*beta*' | tail -n 1)
 if [[ $(git status -s) ]]; then
     echo "Error: Git working directory is not clean. Please commit or discard your changes."
     exit 1
 fi
 bumpversion $1
-version_new=v$(dotnet-gitversion /showvariable MajorMinorPatch)
+version_new=v$(dotnet-gitversion /showvariable SemVer)
 git add .
 git commit -n -m "Bumpversion $version_old -> $version_new"
 git push
