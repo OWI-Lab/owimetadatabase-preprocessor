a88b290d866e800d17f205afe77bd0bbb3f80b18
arsmlnkv <melnikov.arsene@gmail.com>
2024-03-11 11:26:50 +0100

    Initial tests.

--- tests/soil/conftest.py
+++ tests/soil/conftest.py
@@ -1,6 +1,58 @@
+import json
+from unittest import mock
+
 import pytest
+import requests
 
 
-@pytest.fixture
-def api_root_soil():
-    return "http://test.be/api/soil/"
+@pytest.fixture(scope="function")
+def soil_init(header):
+    return {
+        "api_root": "https://owimetadatabase.owilab.be/api/v1/soildata/",
+        "header": header,
+        "auth": None,
+        "uname": None,
+        "password": None,
+    }
+
+
+@pytest.fixture(scope="function")
+def mock_requests_get_proximity_entities_2d(mocker: mock.Mock) -> mock.Mock:
+
+    def custom_side_effect(*args, **kwargs) -> requests.Response:
+        resp = requests.Response()
+        if kwargs.get("params") == {
+            "latitude": 50.1,
+            "longitude": 2.22,
+            "offset": 0.75,
+        }:
+            data = [
+                {
+                    "col_1": 1,
+                    "col_2": 49.5,
+                    "col_3": True,
+                    "col_4": "test_1",
+                    "col_5": {"test_1": 1.0},
+                    "col_6": None,
+                },
+                {
+                    "col_1": 2,
+                    "col_2": 51.1,
+                    "col_3": False,
+                    "col_4": "test_2",
+                    "col_5": {"test_2": 2.0},
+                    "col_6": None,
+                },
+            ]
+            resp.status_code = 200
+        else:
+            data = []
+            resp.status_code = 404
+            resp.reason = (
+                "The object corresponding to the specified parameters is not found"
+            )
+        resp._content = json.dumps(data).encode("utf-8")
+        return resp
+
+    mock = mocker.patch("requests.get", side_effect=custom_side_effect)
+    return mock
--- tests/soil/test_io.py
+++ tests/soil/test_io.py
@@ -1,1 +1,59 @@
-# from owimetadatabase_preprocessor.soil.io import SoilAPI
+from typing import Any, Dict
+from unittest import mock
+
+import pandas as pd
+import pytest
+import requests
+
+from owimetadatabase_preprocessor.soil.io import SoilAPI
+
+
+def test_init(soil_init: Dict[str, Any], header: Dict[str, str]) -> None:
+    api_soil = SoilAPI(header=header)
+    assert soil_init == api_soil
+
+
+def test_process_data(
+    api_root: str, header: Dict[str, str], mock_requests_get_advanced: mock.Mock
+) -> None:
+    header = header
+    url_data_type = "/test/"
+    url_params = {"test": "test"}
+    output_type = "list"
+    api_test = SoilAPI(api_root, header=header)
+    df, df_add = api_test.process_data(url_data_type, url_params, output_type)
+    assert isinstance(df, pd.DataFrame)
+    assert isinstance(df_add, dict)
+    assert isinstance(df_add["existance"], bool)
+    assert isinstance(df_add["response"], requests.Response)
+
+
+def test_get_proximity_entities_2d(
+    api_root: str,
+    header: Dict[str, str],
+    mock_requests_get_proximity_entities_2d: mock.Mock,
+) -> None:
+    api_test = SoilAPI(api_root, header=header)
+    data = api_test.get_proximity_entities_2d(
+        api_url="test", latitude=50.1, longitude=2.22, radius=0.75
+    )
+    df = data["data"]
+    assert isinstance(df, pd.DataFrame)
+    assert isinstance(data["exists"], bool)
+    assert data["exists"]
+    assert len(df) == 2
+    print(df["col_4"], df["col_6"])
+    for col, dtype_ in zip(df.columns, [int, float, bool, object, dict, object]):
+        assert df[col].dtype == dtype_
+
+
+def test_get_proximity_entities_2d_wrong_data(
+    api_root: str,
+    header: Dict[str, str],
+    mock_requests_get_proximity_entities_2d: mock.Mock,
+) -> None:
+    api_test = SoilAPI(api_root, header=header)
+    with pytest.raises(Exception):
+        api_test.get_proximity_entities_2d(
+            api_url="test", latitude=50, longitude=2.22, radius=0.75
+        )
