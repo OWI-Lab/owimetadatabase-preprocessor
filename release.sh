525e3a03ef59c231996a928dd57d921ebd258fb4
arsmlnkv <melnikov.arsene@gmail.com>
2024-03-14 09:38:15 +0100

    Adopted versioning and release script for beta versions.

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 version_old=$(git tag --list '*beta*' | tail -n 1)
-# if [[ $(git status -s) ]]; then
-#     echo "Error: Git working directory is not clean. Please commit or discard your changes."
-#     exit 1
-# fi
-bumpversion $1 --allow-dirty
 version_new=v$(dotnet-gitversion /showvariable SemVer)
+if [[ $(git status -s) ]]; then
+    echo "Error: Git working directory is not clean. Please commit or discard your changes."
+    exit 1
+fi
+bumpversion $1 --allow-dirty
 git add .
 git commit -n -m "Bumpversion $version_old -> $version_new"
 git push
