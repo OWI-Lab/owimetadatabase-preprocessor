3d17c1885d5343403526e4d1ed194afd59053bbe
arsmlnkv <melnikov.arsene@gmail.com>
2024-03-20 12:54:59 +0100

    Bumpversion v0.6.2 -> v0.7.0

--- release.sh
+++ release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-version_old=$(git describe --tags $(git rev-list --tags --max-count=1))
-if [[ $(git status -s) ]]; then
-    echo "Error: Git working directory is not clean. Please commit or discard your changes."
-    exit 1
-fi
-bumpversion $1
+version_old=$(git describe --tags --abbrev=0)
+# if [[ $(git status -s) ]]; then
+#     echo "Error: Git working directory is not clean. Please commit or discard your changes."
+#     exit 1
+# fi
+bumpversion $1 --allow-dirty
 version_new=v$(dotnet-gitversion /showvariable MajorMinorPatch)
 git add .
 git commit -n -m "Bumpversion $version_old -> $version_new"
