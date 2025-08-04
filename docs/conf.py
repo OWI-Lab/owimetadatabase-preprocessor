143222e323d98838b6e5ae8f6f2b07ed01848084
arsmlnkv <melnikov.arsene@gmail.com>
2025-08-04 14:40:48 +0200

    Bumpversion v0.9.4 -> v0.9.5

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, module_path)
 project = "Owimetadatabase preprocessor"
 copyright = "2025, OWI-Lab"
 author = "OWI-Lab"
-version = "0.9.4"
+version = "0.9.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
