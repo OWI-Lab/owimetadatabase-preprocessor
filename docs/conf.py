01ddb4c6c4b2a2a22bf7ed2c8a4a634adcdda853
arsmlnkv <melnikov.arsene@gmail.com>
2025-11-05 09:44:05 +0100

    update: docs theme to 'furo'

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"  # "alabaster"
+html_theme = "furo"  # "alabaster" "sphinx_rtd_theme"
 autodoc_default_options = {
     'private-members': False
 }
